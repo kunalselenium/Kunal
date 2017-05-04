845cf296176431aff1687662be56c0b9af16ddb6
Araya.Kunal <Araya.Kunal@ArayaKunal-PC>
2017-05-04 05:58:40 +0530

    Commited

---
+++ hello/src/test/java/com/firstProject/selenium/MavenProject.java
@@ -0,0 +1,11 @@
+package com.firstProject.selenium;
+
+import org.testng.annotations.Test;
+
+public class MavenProject {
+	@Test
+	public void mavenFirstProject(){
+		System.out.println("hello world");
+	}
+
+}
