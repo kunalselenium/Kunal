c0a8e2468b446b7e57ca9e8d52902556d79a6783
Araya.Kunal <Araya.Kunal@ArayaKunal-PC>
2017-05-04 07:39:52 +0530

    some changes made

--- hello/src/test/java/com/firstProject/selenium/MavenProject.java
+++ hello/src/test/java/com/firstProject/selenium/MavenProject.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class MavenProject {
 	@Test
 	public void mavenFirstProject(){
-		System.out.println("hello world");
+		System.out.println("hello word");
 	}
 
 }
